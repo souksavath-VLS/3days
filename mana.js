8e5d45a5aede0d187968d1b55dc49cccc3b37b77
souksavath-VLS <79986716+souksavath-VLS@users.noreply.github.com>
2022-01-22 08:56:18 +0700

    first commit

--- mana.js
+++ mana.js
@@ -1,1 +1,14 @@
-MediaStreamAudioSourceNode
+let z = 5;
+
+
+function Calculator(x, y) {
+  setTimeout(() => {
+
+    
+    z = x + y;
+    console.log('Z =' ,z)
+  }, 2000);
+}
+
+Calculator(5 , 5);
+console.log('==================================== Display ========================================')
